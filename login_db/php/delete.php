f13f9204153eee987711861d7213cb846a9f12cb
EMON, RASEL IQBAL <58358306+emon-iqbal@users.noreply.github.com>
2020-03-31 16:49:22 +0600

    Update delete.php

--- login_db/php/delete.php
+++ login_db/php/delete.php
@@ -1,27 +1,13 @@
 <?php 
 	
-	
 		$con = mysqli_connect('127.0.0.1', 'root', '', 'webtech');
 		
-		/*if(!$con)
-		{
-			echo 'connection to server is denied';
-		}
-		*/
-		
-        $sql = "DELETE FROM 'users' WHERE ID='$_POST[id]', USERNAME='$_POST[username]', PASSWORD='$_POST[password]', TYPE='$_POST[type]', EmaiL='$_POST[email]'"; 
+ 
+        $sql = "DELETE  FROM users WHERE ID='$_GET[id]'";
         
-		if(mysqli_query($con, $sql))
-		{
-			header("refresh:5;	url=../views/delete_users.php");
-			
-		}
+		if(mysqli_query($con,$sql))
+			header("refresh:5;	url=../views/USER_LIST.php");
 		else
-		{
 			echo "!! Error !!";
-		}
-		
-		header("refresh:3;	url=../views/home.php");
-	
 	
 ?>
